4d7bf272e7be9641a8a538d2c1b8e1a3df9a7e4b
GhostLead <72067053+GhostLead@users.noreply.github.com>
2024-02-23 11:08:56 +0100

    asd

---
+++ ConsoleAppMasodikFeladatNeumann/ConsoleAppMasodikFeladatNeumann/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Principal;
+
+namespace ConsoleAppMasodikFeladatNeumann
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Telepules> telepulesek = File.ReadAllLines("telepules.txt").Select(x => new Telepules(x)).ToList();
+            List<Megyek> megyek = File.ReadAllLines("megyek.txt").Select(x => new Megyek(x)).ToList();
+            Dictionary<string,int> megyeklakosai = new Dictionary<string,int>();
+
+            // 2a)
+            List<Telepules> telepulesekRendezve = telepulesek.OrderBy(x => x.LakosokSzama).ToList();
+            telepulesekRendezve.RemoveAt(0);
+            string megyenev = megyek.Find(x => x.Kod == telepulesekRendezve[0].MegyeAzonosito).Nev;
+            foreach (var telepules in telepulesek)
+            {
+                if (megyeklakosai.ContainsKey(megyek.Find(x => x.Kod == telepules.MegyeAzonosito).Nev))
+                {
+                    continue;
+                }
+                else
+                {
+                    megyeklakosai.Add(megyek.Find(x => x.Kod == telepules.MegyeAzonosito).Nev, 0);
+                }
+                
+            }
+            int lakosszam = 0;
+            foreach (var item in megyeklakosai)
+            {
+                foreach (var telepules in telepulesek)
+                {
+                    if (megyek.Find(x => x.Kod == telepules.MegyeAzonosito).Nev == item.Key)
+                    {
+                        megyeklakosai[megyek.Find(x => x.Kod == telepules.MegyeAzonosito).Nev] += telepules.LakosokSzama;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            megyeklakosai.Remove(megyeklakosai.Keys.First());
+            Console.WriteLine($"{megyeklakosai.OrderBy(x => x.Value).First().Key}-{megyeklakosai.OrderBy(x => x.Value).First().Value}");
+
+            //Console.WriteLine($"{megyenev}-{telepulesekRendezve.First().LakosokSzama}");
+
+            // 2b)
+            
+
+        }
+    }
+}
---
+++ ConsoleAppNajmannnn/ConsoleAppNajmannnn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleAppNajmannnn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Szamok = File.ReadAllLines("szamok.txt").ToList();
+
+            // 1c)
+
+            List<int> ketjegyuek = new List<int>();
+
+            foreach (string szam in Szamok)
+            {
+                for (int i = 0; i < szam.Length-1; i++)
+                {
+                    try
+                    {
+                        ketjegyuek.Add(Convert.ToInt32($"{szam[i]}{szam[i + 1]}"));
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            int megoldas = ketjegyuek.GroupBy(x => x).OrderByDescending(group => group.Count()).Select(group => group.Key).First();
+            Console.WriteLine(megoldas);
+
+        }
+    }
+}
